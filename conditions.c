3a90b87445e862edd6a8fa98f5400cc6859f84f6
neeraj733 <92708575+neeraj733@users.noreply.github.com>
2021-11-08 11:57:22 +0530

    Add files via upload

---
+++ conditions.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	a=20;
+	b=10;
+	printf("%d>%d is %d\n",a,b,a>b);
+	printf("%d>=%d is %d\n",a,b,a>=b);
+	printf("%d<%d is %d\n",a,b,a<b);
+	printf("%d<=%d is %d\n",a,b,a<=b);
+	printf("%d==%d is %d\n",a,b,a==b);
+	printf("%d!=%d is %d\n",a,b,a!=b);
+
+}
