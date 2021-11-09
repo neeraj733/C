562cf2d402cec11614115c453073bf37f94411f7
neeraj733 <92708575+neeraj733@users.noreply.github.com>
2021-11-09 11:55:24 +0530

    Add files via upload

---
+++ add,sub,prod.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c,d;
+	printf("enter 3 numbers:");
+	scanf("%d%d%d%d",&a,&b,&c,&d);
+	printf("number 1 is : %d\n",a);
+	printf("number 2 is : %d\n",b);
+	printf("number 3 is : %d\n",c);
+	printf("number 4 is : %d\n",d);
+	printf("prodect of 4 numbers is %d\n",a*b*c*d);
+	printf("sum of 4 numbers is : %d\n",a+b+c+d);
+	printf("substaction of 4 numbers is %d",a-b-c-d);
+	
+}
