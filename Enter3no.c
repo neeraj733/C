6b13e006c8c3637a3678da02c94b86fe9cc1ceed
neeraj733 <92708575+neeraj733@users.noreply.github.com>
2021-11-09 11:38:05 +0530

    Add files via upload

---
+++ Enter3no.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	printf("enter 3 numbers:");
+	scanf("%d%d%d",&a,&b,&c);
+	printf("number 1 is : %d\n",a);
+	printf("number 2 is : %d\n",b);
+	printf("number 3 is : %d",c);
+}
