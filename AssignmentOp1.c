b520d9dfa92059667176132f8e6a1f98db27ac66
neeraj733 <92708575+neeraj733@users.noreply.github.com>
2021-11-17 12:07:37 +0530

    Add files via upload

---
+++ AssignmentOp1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	a=10;
+	b=2;
+	a+=b;
+	b-=a;
+	a*=b;
+	a/=b;
+	a%=b;
+	printf("%d%d",a,b);
+	
+}
---
+++ AssignmentOp2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int a=10,b=4,c=3;
+	a+=b;
+	b-=c;
+	c*=a;
+	a/=b;
+	b*=c;
+	a+=b;
+	printf("%d %d %d",a,b,c);
+}
+
