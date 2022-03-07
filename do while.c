736280721bf172448545b118bdf243fb5943a367
neeraj733 <92708575+neeraj733@users.noreply.github.com>
2022-03-07 12:19:16 +0530

    Add files via upload

---
+++ do while.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int i=5;
+	do
+	{
+	printf("%d\n",i);
+	i+=5;
+}
+while(i<=50);
+return 0;
+}
+
---
+++ while.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int i=5;
+	while(i<=50)
+	{
+	printf("%d\n",i);
+	i+=5;
+}
+return 0;
+}
